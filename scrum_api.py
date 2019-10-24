fb1cf14b0257307842d99d8d3becabc4a77f0b9a
Anurag <a.baddam@salesforce.com>
2019-10-24 00:57:02 -0700

    initial commit

---
+++ scrum_api.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class UpdateGus(Resource):
+    def post(self):
+        json = request.get_json()
+        return {"you sent": json}
+
+
+api.add_resource(UpdateGus, "/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
