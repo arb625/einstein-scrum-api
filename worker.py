5ea9a58e217b916a0570ec67ca8a8b058482026e
Anurag <a.baddam@salesforce.com>
2019-10-25 10:12:08 -0700

    added more error handling 2

--- worker.py
+++ worker.py
@@ -3,13 +3,17 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 
-listen = ['default']
-
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-
-conn = redis.from_url(redis_url)
+try:
+    listen = ['default']
+    redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+    conn = redis.from_url(redis_url)
+except:
+    pass
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(list(map(Queue, listen)))
-        worker.work()
+    try:
+        with Connection(conn):
+            worker = Worker(list(map(Queue, listen)))
+            worker.work()
+    except:
+        pass    
