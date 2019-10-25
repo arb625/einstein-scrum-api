9ccc2e2bb9ff9a7d666291570aa59396a2230029
Anurag <a.baddam@salesforce.com>
2019-10-25 01:10:46 -0700

    filter for only 11a sprint

--- gus_action.py
+++ gus_action.py
@@ -11,13 +11,15 @@ sf = Salesforce(organizationId=GUS_ORG_ID, username=username, password=password)
 GUS_ENTITY_WORK_ITEM = "ADM_Work__c"
 GUD_FIELD_WORK_ID_AND_SUBJECT = "WorkId_and_Subject__c"
 GUS_FIELD_STATUS = "Status__c"
-
+GUS_FIELD_SPRINT = "Sprint__c"
+SBD_11A_SPRINT = "a0lB0000001qmeYIAQ"
 
 def update_status(work_id, new_status):
     entity_id = sf.query(f"""
         SELECT Id
         FROM {GUS_ENTITY_WORK_ITEM}
         WHERE {GUD_FIELD_WORK_ID_AND_SUBJECT} LIKE '%{work_id}%'
+        AND {GUS_FIELD_SPRINT} = '{SBD_11A_SPRINT}'
     """)["records"][0]["Id"]
     ret = sf.ADM_Work__c.update(entity_id, {GUS_FIELD_STATUS: new_status})
     return ret
