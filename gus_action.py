435216ebdd630eafa95adc07315b872dc31f8a4a
Anurag <a.baddam@salesforce.com>
2019-10-25 01:24:58 -0700

    edge case with no work id

--- gus_action.py
+++ gus_action.py
@@ -14,11 +14,14 @@ GUS_FIELD_STATUS = "Status__c"
 GUS_FIELD_SPRINT = "Sprint__c"
 SBD_11A_SPRINT = "a0lB0000001qmeYIAQ"
 
+
 def update_status(work_id, new_status):
+    if not work_id or len(work_id) == 0:
+        return 400
     entity_id = sf.query(f"""
         SELECT Id
         FROM {GUS_ENTITY_WORK_ITEM}
-        WHERE {GUD_FIELD_WORK_ID_AND_SUBJECT} LIKE '%{work_id}%'
+        WHERE {GUD_FIELD_WORK_ID_AND_SUBJECT} LIKE '%{work_id}'
         AND {GUS_FIELD_SPRINT} = '{SBD_11A_SPRINT}'
     """)["records"][0]["Id"]
     ret = sf.ADM_Work__c.update(entity_id, {GUS_FIELD_STATUS: new_status})
